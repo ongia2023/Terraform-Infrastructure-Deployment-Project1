4c3b62f5037a1fcf76b91e4f2baae98e496e1327
Ongia Emmanuel <126395220+ongia2023@users.noreply.github.com>
2023-12-16 17:14:48 +0300

    Add files via upload

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ module "vpc" {
 module "ec2_instance" {
   source            = "./modules/ec2_instance"
   regions           = var.regions
-  environments      = ["dev"]
+  environments      = ["prod"]
   instance_type     = var.instance_type
-  ami               = var.ami["dev"]
+  ami               = var.ami["prod"]
   vpc_id            = module.vpc.vpc_id
   subnet_ids        = module.vpc.subnet_ids
   availability_zones = module.vpc.availability_zones
